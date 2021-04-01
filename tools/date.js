fdaa87868df230d3a1f3859a18888fd807a3636e
Ben Rowe <brow495@gmail.com>
2021-04-01 14:15:00 -0600

    Fix date formatting.

--- tools/date.js
+++ tools/date.js
@@ -1,14 +1,16 @@
 export default function getDateString() {
   const date = new Date()
+  
   let month = date.getMonth() + 1
+  month = month > 9 ? `${month}` : `0${month}`
+  
   let day = date.getUTCDate()
+  day = day > 9 ? `${day}` : `0${day}`
+
   let year = date.getUTCFullYear() 
+  
   return {
-    url: (
-      month > 9
-      ? `${year}${month}${day}`
-      : `${year}0${month}${day}`
-    ),
+    url: `${year}${month}${day}`,
     header: date.toDateString().slice(4)
   }
 }
